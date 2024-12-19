272f0ca559c8b1c85eac3f652785eb8a33483dbe
NithinSai11421 <153480138+NithinSai11421@users.noreply.github.com>
2024-12-19 20:52:52 +0530

    version 2

--- v1.py
+++ v1.py
@@ -1,7 +1,6 @@
-a=5
-b=4
-c=6
-x=4.5
-y=0
-y = a*(x*x)+b*x+c;
-print("for the values:a",a,"b",b,"c",c,"y is:",y)
+a=float(input('Enter the value of a: '))
+b=float(input('Enter the value of b: '))
+c=float(input('Enter the value of c: '))
+x=float(input('Enter the value of x: '))
+y=a*x*x+b*x+c
+print('The value of y is ',y)
